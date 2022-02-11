215039c30e75c1c9a66a1484b0e25886c167fd00
Ronaldo-max <ronaldodcs21@gmail.com>
2022-02-10 23:14:32 -0300

    update on interface

--- script.js
+++ script.js
@@ -1,59 +1,41 @@
-const remover = document.getElementById("remove");
-const add = document.getElementById("add");
 const playPomodoro = document.getElementById("play");
-
 const circle = document.querySelector("circle");
 const audio = document.querySelector("audio");
-
 const buttonAudio = document.getElementById("buttonAudio");
+const reset = document.getElementById("reset");
 
-let timer = 25;
-
+let timer = 1;
 let minutes = timer * 60;
 let seconds = minutes % 60;
+
+let interval = 1;
+let minutesInterval = interval * 60;
 
 let circleValue = 875;
 let circleValueInterval;
 
-function updateInnerHtml (){
-    document.querySelector("#timer p").innerHTML = `${
-        minutes / 60 >= 10 ? minutes / 60 : "0" + minutes / 60
-    }:${seconds < 10 ? "0" + seconds : seconds}`;
-}
-
 let handle;
 
-const ActionsPlay = {
-    removeButtons() {
-        remover.style.display = "none"
-        add.style.display = "none"
-    },
-
-    addButtons() {
-        remover.style.display = "flex"
-        add.style.display = "flex"
-    },
-}
-
 const Update = {
-    updateCircle() {
-        circle.style.strokeDashoffset = circleValue += circleValueInterval;
-    },
-
-    updateValues() {
-        let minutesValue = Math.floor(minutes / 60);
-        let secondsValue = minutes % 60;
-
-        document.querySelector("#timer p").innerHTML = `${
-            minutesValue >= 10 ? minutesValue : "0" + minutesValue
-        }:${secondsValue < 10 ? "0" + secondsValue : secondsValue}`;
-    },
-
     updateMinutes() {
         if (minutes > 0) {
             minutes--;
-            Update.updateCircle();
+            Update.updateInnerHtml();
+            Update.updateCircle()
         }
+    },
+
+    updateInnerHtml (){
+        let min = Math.floor(minutes / 60)
+        let sec = minutes %  60
+    
+        document.querySelector("#timer p").innerHTML =
+            `${ min >= 10 ? min : "0" + min }:${sec < 10 ? "0" + sec : sec}`;
+    },
+
+    updateCircle() {
+        circleValueInterval = 875 / minutes
+        circle.style.strokeDashoffset = circleValue += circleValueInterval;
     },
 };
 
@@ -70,40 +52,31 @@ const AudioPlay = {
 };
 
 const ActionButtons = {
-    add() {
-        minutes = minutes + 60;
-        updateInnerHtml()
-    },
-
-    remove() {
-        if (minutes > 0) {
-            minutes = minutes - 60;
-            updateInnerHtml()
-
-        } else {
-            alert("Valor mínimo alcançado");
-        }
-    },
-
     activeTimer() {
-        ActionsPlay.removeButtons();
-        circleValueInterval = 875 / minutes
         handle = setInterval(() => {
             if (minutes > 0) {
                 Update.updateMinutes();
-                Update.updateValues();
+                Update.updateInnerHtml();
             } else if (minutes === 0) {
                 AudioPlay.playAudio();
-                ActionsPlay.addButtons();
             }
         }, 1000);
-    }
+    },
+
+    reset() {
+        clearInterval(handle);
+
+        circleValueInterval = 0;
+        circleValue = 875;
+        circle.style.strokeDashoffset = 0;
+
+        minutes = 25 * 60;
+        Update.updateInnerHtml();
+    },
 };
 
-updateInnerHtml();
+Update.updateInnerHtml();
 
 buttonAudio.addEventListener("click", AudioPlay.pauseAudio);
-
-add.addEventListener("click", ActionButtons.add);
-remover.addEventListener("click", ActionButtons.remove);
 playPomodoro.addEventListener("click", ActionButtons.activeTimer);
+reset.addEventListener("click", ActionButtons.reset);
