d305ee474e02ad198f30c5d8c9c2ad98b03ca7c4
Ronaldo-max <ronaldodcs21@gmail.com>
2022-02-11 15:05:03 -0300

    fixed interval in script.js

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const circle = document.querySelector("circle");
 const audio = document.querySelector("audio");
 const buttonAudio = document.getElementById("buttonAudio");
 const reset = document.getElementById("reset");
+const textInterval = document.getElementById("interval");
 
 const cicles = document.getElementById("cicles");
 
-let timer = 1;
+let timer = 25;
 let minutesFixed = timer * 60;
 let minutes = timer * 60;
 let seconds = minutes % 60;
 
 let ciclesTime = 0;
 
-let interval = 1;
+let interval = 5;
+let minutesIntervalFixed = interval * 60;
 let minutesInterval = interval * 60;
 
 let circleValue = 875;
@@ -21,7 +23,8 @@ let circleValueInterval;
 
 circleValueInterval = 875 / minutes
 
-let updateTimeSetInterval;
+let updateTimerMinutesSeconds;
+let updateTimerInterval;
 
 const Update = {
     updateMinutes() {
@@ -31,18 +34,43 @@ const Update = {
         }
     },
 
+    updateInterval() {
+        if (minutesInterval > 0) {
+            minutesInterval--;
+            Update.updateInnerHtml();
+        }
+    },
+
     updateInnerHtml (){
         let min = Math.floor(minutes / 60)
         let sec = minutes % 60
+
+        let intervalMin = Math.floor(minutesInterval / 60)
+        let intervalSec = minutesInterval % 60
     
         document.querySelector("#timer p").innerHTML =
-            `${ min >= 10 ? min : "0" + min }:${sec < 10 ? "0" + sec : sec}`;
+            `${ min >= 10 ? min : "0" + min}:${sec < 10 ? "0" + sec : sec}`;
 
         document.querySelector("#cicles").innerHTML = ciclesTime;
+
+        textInterval.innerHTML =
+        `${ intervalMin >= 10 ? intervalMin : "0" + intervalMin}:${intervalSec < 10 ? "0" + intervalSec : intervalSec}`;
     },
 
     updateCircle() {
         circle.style.strokeDashoffset = circleValue += circleValueInterval;
+    },
+
+    updateIntervalTimer() {
+        updateTimerInterval = setInterval(() => {
+            if (minutes === 0 && minutesInterval > 0) {
+                Update.updateInterval();
+                Update.updateInnerHtml();
+            } else if (minutesInterval === 0){
+                AudioPlay.playAudio();
+                clearInterval(updateTimerInterval);
+            }
+        }, 1000)
     },
 };
 
@@ -52,34 +80,51 @@ const AudioPlay = {
     },
     pauseAudio() {
         audio.pause();
+
+        if (minutes == 0 && minutesInterval > 0) {
+            Update.updateIntervalTimer();
+        } else if (minutesInterval == 0 && minutes == 0) {
+            minutesInterval = minutesIntervalFixed;
+            minutes = minutesFixed;
+            Update.updateInnerHtml();
+            // UPDATE CIRCLE
+            circleValueInterval = 875 / minutes;
+            circleValue = 875;
+            circle.style.strokeDashoffset = 0;
+        }
     },
 };
 
 const ActionButtons = {
     activeTimer() {
-        updateTimeSetInterval = setInterval(() => {
+        updateTimerMinutesSeconds = setInterval(() => {
             if (minutes > 0) {
                 Update.updateMinutes();
                 Update.updateInnerHtml();
                 Update.updateCircle()
             } else if (minutes === 0) {
-                    clearInterval(updateTimeSetInterval)
-                    AudioPlay.playAudio();
-                    ciclesTime += 1
-                    minutes = minutesInterval;
-                    Update.updateInnerHtml();
+                clearInterval(updateTimerMinutesSeconds)
+                AudioPlay.playAudio();
+                ciclesTime += 1;
+                Update.updateInnerHtml();
+
+                circleValueInterval = 0;
+                circleValue = 875;
+                circle.style.strokeDashoffset = 0;
             }
         }, 1000);
     },
 
     reset() {
-        clearInterval(updateTimeSetInterval);
+        clearInterval(updateTimerMinutesSeconds);
+        clearInterval(updateTimerInterval);
 
         circleValueInterval = 0;
         circleValue = 875;
         circle.style.strokeDashoffset = 0;
 
         minutes = minutesFixed;
+        minutesInterval = minutesIntervalFixed;
         Update.updateInnerHtml();
     },
 };
