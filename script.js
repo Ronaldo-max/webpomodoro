6695bd391c028774bf2dafe3160ce8c0a755cc35
Ronaldo-max <ronaldodcs21@gmail.com>
2022-05-02 08:39:41 -0300

    format prettier

--- script.js
+++ script.js
@@ -26,108 +26,109 @@ let updateTimerMinutesSeconds;
 let updateTimerInterval;
 
 const Update = {
-    updateMinutes() {
-        if (minutes > 0) {
-            minutes--;
-            Update.updateInnerHtml();
-        }
-    },
+  updateMinutes() {
+    if (minutes > 0) {
+      minutes--;
+      Update.updateInnerHtml();
+    }
+  },
 
-    updateInterval() {
-        if (minutesInterval > 0) {
-            minutesInterval--;
-            Update.updateInnerHtml();
-        }
-    },
+  updateInterval() {
+    if (minutesInterval > 0) {
+      minutesInterval--;
+      Update.updateInnerHtml();
+    }
+  },
 
-    updateInnerHtml() {
-        let min = Math.floor(minutes / 60);
-        let sec = minutes % 60;
+  updateInnerHtml() {
+    let min = Math.floor(minutes / 60);
+    let sec = minutes % 60;
 
-        let intervalMin = Math.floor(minutesInterval / 60);
-        let intervalSec = minutesInterval % 60;
+    let intervalMin = Math.floor(minutesInterval / 60);
+    let intervalSec = minutesInterval % 60;
 
-        document.querySelector("#timer p").innerHTML = `${
-            min >= 10 ? min : "0" + min
-        }:${sec < 10 ? "0" + sec : sec}`;
+    document.querySelector("#timer p").innerHTML = `${
+      min >= 10 ? min : "0" + min
+    }:${sec < 10 ? "0" + sec : sec}`;
 
-        document.querySelector("#cicles").innerHTML = ciclesTime;
+    document.querySelector("#cicles").innerHTML = ciclesTime;
 
-        textInterval.innerHTML = `${
-            intervalMin >= 10 ? intervalMin : "0" + intervalMin
-        }:${intervalSec < 10 ? "0" + intervalSec : intervalSec}`;
-    },
+    textInterval.innerHTML = `${
+      intervalMin >= 10 ? intervalMin : "0" + intervalMin
+    }:${intervalSec < 10 ? "0" + intervalSec : intervalSec}`;
+  },
 
-    updateCircle() {
-        circle.style.strokeDashoffset = circleValue += circleValueInterval;
-    },
+  updateCircle() {
+    circle.style.strokeDashoffset = circleValue += circleValueInterval;
+  },
 
-    updateIntervalTimer() {
-        updateTimerInterval = setInterval(() => {
-            if (minutes === 0 && minutesInterval > 0) {
-                Update.updateInterval();
-                Update.updateInnerHtml();
-            } else if (minutesInterval === 0) {
-                AudioPlay.playAudio();
-                clearInterval(updateTimerInterval);
-            }
-        }, 1000);
-    },
+  updateIntervalTimer() {
+    updateTimerInterval = setInterval(() => {
+      if (minutes === 0 && minutesInterval > 0) {
+        Update.updateInterval();
+        Update.updateInnerHtml();
+      } else if (minutesInterval === 0) {
+        AudioPlay.playAudio();
+        clearInterval(updateTimerInterval);
+      }
+    }, 1000);
+  },
 };
 
 const AudioPlay = {
-    playAudio() {
-        audio.play();
-    },
-    pauseAudio() {
-        audio.pause();
+  playAudio() {
+    audio.play();
+  },
+  pauseAudio() {
+    audio.pause();
 
-        if (minutes == 0 && minutesInterval > 0) {
-            Update.updateIntervalTimer();
-        } else if (minutesInterval == 0 && minutes == 0) {
-            minutesInterval = minutesIntervalFixed;
-            minutes = minutesFixed;
-            Update.updateInnerHtml();
-            // UPDATE CIRCLE
-            circleValueInterval = 875 / minutes;
-            circleValue = 875;
-            circle.style.strokeDashoffset = 0;
-        }
-    },
+    if (minutes == 0 && minutesInterval > 0) {
+      Update.updateIntervalTimer();
+    } else if (minutesInterval == 0 && minutes == 0) {
+      minutesInterval = minutesIntervalFixed;
+      minutes = minutesFixed;
+      Update.updateInnerHtml();
+      // UPDATE CIRCLE
+      circleValueInterval = 875 / minutes;
+      circleValue = 875;
+      circle.style.strokeDashoffset = 0;
+    }
+  },
 };
 
 const ActionButtons = {
-    activeTimer() {
-        updateTimerMinutesSeconds = setInterval(() => {
-            if (minutes > 0) {
-                Update.updateMinutes();
-                Update.updateInnerHtml();
-                Update.updateCircle();
-            } else if (minutes === 0) {
-                clearInterval(updateTimerMinutesSeconds);
-                AudioPlay.playAudio();
-                ciclesTime += 1;
-                Update.updateInnerHtml();
-
-                circleValueInterval = 0;
-                circleValue = 875;
-                circle.style.strokeDashoffset = 0;
-            }
-        }, 1000);
-    },
-
-    reset() {
+  activeTimer() {
+    updateTimerMinutesSeconds = setInterval(() => {
+      if (minutes > 0) {
+        Update.updateMinutes();
+        Update.updateInnerHtml();
+        Update.updateCircle();
+      } else if (minutes === 0) {
         clearInterval(updateTimerMinutesSeconds);
-        clearInterval(updateTimerInterval);
+        AudioPlay.playAudio();
+        ciclesTime += 1;
+        Update.updateInnerHtml();
 
         circleValueInterval = 0;
         circleValue = 875;
         circle.style.strokeDashoffset = 0;
+      }
+    }, 1000);
+  },
 
-        minutes = minutesFixed;
-        minutesInterval = minutesIntervalFixed;
-        Update.updateInnerHtml();
-    },
+  reset() {
+    clearInterval(updateTimerMinutesSeconds);
+    clearInterval(updateTimerInterval);
+
+    circleValueInterval = 0;
+    circleValue = 875;
+    circle.style.strokeDashoffset = 0;
+    ciclesTime = 0;
+
+    minutes = minutesFixed;
+    minutesInterval = minutesIntervalFixed;
+    Update.updateInnerHtml();
+  },
 };
 
 Update.updateInnerHtml();
